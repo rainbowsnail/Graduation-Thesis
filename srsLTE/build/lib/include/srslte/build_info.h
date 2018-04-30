22fca016ab90ff32f48e8b9e069452f1f30cddfc
jwtty <jwtty@pku.edu.cn>
2018-05-01 00:19:59 +0800

    asn file finish

--- srsLTE/build/lib/include/srslte/build_info.h
+++ srsLTE/build/lib/include/srslte/build_info.h
@@ -39,7 +39,7 @@ extern "C" {
 #endif
 
 // the configured build options for srsLTE
-static char build_info[] = "commit 12ab591 on branch master";
+static char build_info[] = "commit 67562f2 on branch master";
 
 SRSLTE_API char* srslte_get_build_info() {
   return build_info;
